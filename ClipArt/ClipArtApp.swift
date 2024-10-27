7980da446e56a08be149846d2e08a4ca49a5b514
Hurynovič Andrej <dpkdpk10@icloud.com>
2024-10-27 16:33:41 +0300

    Refactor ClipArtApp.

--- ClipArt/ClipArtApp.swift
+++ ClipArt/ClipArtApp.swift
@@ -5,47 +5,13 @@
 //
 
 import SwiftUI
-import AppKit
 
 @main
 struct ClipArtApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
     var body: some Scene {
-        Settings {
-            EmptyView() // No settings window by default; handled by the status bar.
+        MenuBarExtra("ClipArt", systemImage: "clipboard") {
+            ContentView()
         }
-    }
-}
-
-@MainActor
-class AppDelegate: NSObject, NSApplicationDelegate {
-    var statusItem: NSStatusItem?
-    var popover: NSPopover!
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Create the status item
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        if let button = statusItem?.button {
-            button.image = NSImage(systemSymbolName: "clipboard", accessibilityDescription: "ClipArt") // Set your icon here
-            button.action = #selector(togglePopover(_:))
-        }
-
-        // Create the popover
-        popover = NSPopover()
-        popover.contentSize = NSSize(width: 300, height: 400)
-        popover.behavior = .transient
-        popover.contentViewController = NSHostingController(rootView: ContentView())
-    }
-
-    @objc func togglePopover(_ sender: AnyObject?) {
-        if let button = statusItem?.button {
-            if popover.isShown {
-                popover.performClose(sender)
-            } else {
-                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
-                popover.contentViewController?.view.window?.becomeKey()
-            }
-        }
+        .menuBarExtraStyle(.window)
     }
 }
